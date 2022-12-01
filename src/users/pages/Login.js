8c15b375841c97bde74a20b28f2fa922b3dce7cd
SeokWon <tjrdnjs1534@naver.com>
2022-12-01 19:44:26 +0900

    feat: coars

--- src/users/pages/Login.js
+++ src/users/pages/Login.js
@@ -1,11 +1,49 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    userID : '',
+    password : ''
+  });
+
+  const loginSubmitHandler = (event) => {
+    event.preventDefault();
+    
+    fetch("http://localhost:3000/auth/login", {
+      
+      method: "POST",
+      headers: {
+        'Content-Type' : 'application/json',
+        'Access-Control-Allow-Origin' : 'http://localhost'
+      },
+      credentials: 'include',
+      body: JSON.stringify({ //보내는부분 백이 body에 뭐가 오면 처리하게되어있음
+        userID: formData.userID,
+        password: formData.password
+      }),
+    });
+    console.log()
+  };
+  const idChangeHandler =(event) =>{
+    setFormData({
+        ...formData,
+        userID: event.target.value,
+        
+    }) 
+  }
+
+  const passwordChangeHandler =(event) =>{
+    setFormData({
+        ...formData,
+        password: event.target.value,
+    }) 
+  }
   return (
     <div>
-      <input type="email"></input>
-      <input type="password"></input>
-      <button>login</button>
+      <input type="text" onChange={idChangeHandler}></input>
+      <input type="password" onChange={passwordChangeHandler}></input>
+      <button onClick={loginSubmitHandler}>login</button >
       <Link to={"/sign-up"}>
         <button> sign-up</button>
       </Link>
