de535d5dc6ae430f16decc736401f64cb16ad4fc
SeokWon <tjrdnjs1534@naver.com>
2022-12-01 19:44:26 +0900

    feat: coars

--- src/users/pages/Login.js
+++ src/users/pages/Login.js
@@ -1,60 +1,55 @@
-import React, { useState } from "react";
-import { fetchLogin } from "./service";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Login.css"
 
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    userID : '',
+    password : ''
+  });
 
-  
-  //글로벌 전역 상태값 setUser를 받아옴
-  //로그인이 성공적으로 이루어지면 user에 상태값을 넣어줘야지 나중에 다른 컴포넌트에서도 user값을 이용하여 다른 것들을 할 수 있음
-    const [user, setUser]  = useState({
-      id: "",
-      password: "",
+  const loginSubmitHandler = (event) => {
+    event.preventDefault();
+    
+    fetch("http://localhost:3000/auth/login", {
+      
+      method: "POST",
+      headers: {
+        'Content-Type' : 'application/json',
+        'Access-Control-Allow-Origin' : 'http://localhost'
+      },
+      credentials: 'include',
+      body: JSON.stringify({ //보내는부분 백이 body에 뭐가 오면 처리하게되어있음
+        userID: formData.userID,
+        password: formData.password
+      }),
     });
-  
-    //url 이동을 위한 useHistory\
-  
-    //input에서 입력한 아이디와 비밀번호 정보를 담기위한 state
-    const [account, setAccount] = useState({
-      id: "",
-      password: "",
-    });
-  
-    //input에 입력하면 자동적으로 account state값 변경
-    const onChangeAccount = (e) => {
-        //...을 이용하여 account의 복사본을 만들고
-        //input에 지정한 네임 속성에 해당 value 값을 넣어 오버라이딩!
-        //console.log(account)를 찍어보고 입력한 값들이 account에 출력되면 성공!!
-      setAccount({
-        ...account,
-        [e.target.name]: e.target.value,
-      });
-    };
-  
-    //동기식으로 로그인정보를 통신하여 출력
-    const onSubmitAccount = async () => {
-      try {
-        const user = await fetchLogin(account);
-  
-        //성공하면 해당 user 아이디 패스워드값 셋팅
-        setUser(user);
-        <Link to="/" end>
-        </Link>
-      } catch (error) {
-  
-          //실패하면 throw new Error("") 값 출력
-        window.alert(error);
-      }
-    };
-
-    return (
-      <div className="Container">
-        <input className="Input" placeholder="아이디를 입력해주세요" onChange={onChangeAccount}/> 
-        <input className="Input" type="password" placeholder="비밀번호를 입력해주세요" onChange={onChangeAccount}/>
-        <button className="Button" onClick={onSubmitAccount}>로그인</button>
-      </div>
-    );
+    console.log()
+  };
+  const idChangeHandler =(event) =>{
+    setFormData({
+        ...formData,
+        userID: event.target.value,
+        
+    }) 
   }
-  export default Login;
+
+  const passwordChangeHandler =(event) =>{
+    setFormData({
+        ...formData,
+        password: event.target.value,
+    }) 
+  }
+  return (
+    <div>
+      <input type="text" onChange={idChangeHandler}></input>
+      <input type="password" onChange={passwordChangeHandler}></input>
+      <button onClick={loginSubmitHandler}>login</button >
+      <Link to={"/sign-up"}>
+        <button> sign-up</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Login;
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -2,37 +2,34 @@ import { useEffect } from "react";
 import { useState } from "react";
 import UserList from "../components/UserList";
 const Users = () => {
-     const[users,setUsers]=useState([]);
-    // const fetchUserHandler = () =>{
-    //     fetch("http://localhost:3000/users").then(response =>{
-    //         return response.json()
-    //     }).then(data =>{
-    //         // const transformedUsers = data.results
-    //         setUsers(data)
-    //         console.log(data);
-    //     }) 
-    // } 
-    useEffect(() =>{
-        fetch("http://localhost:3000/users")
-        .then(response =>{
-            return response.json()
-        }).then(response => {
-            if (response.TOKEN) {
-            localStorage.setItem('token', `${response.TOKEN}`);
-            localStorage.setItem('username', `${response.username}`);
-            this.props.history.push('/');
-            } else { alert('ID , password 확인');
-            }
-        })
-        .then(data =>{
-            setUsers(data)
-        }) 
-    } ,[])
-    return (
-        <div>
-            <UserList items={users} />
-        </div>
-    );
-}
+  const [users, setUsers] = useState([]);
+  // const fetchUserHandler = () =>{
+  //     fetch("http://localhost:3000/users").then(response =>{
+  //         return response.json()
+  //     }).then(data =>{
+  //         // const transformedUsers = data.results
+  //         setUsers(data)
+  //         console.log(data);
+  //     })
+  // }
+  useEffect(() => {
+    fetch("http://localhost:3000/users", {
+      headers: { "Access-Control-Allow-Origin": "http://localhost" },
+      credentials: 'include',
 
-export default Users
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+  return (
+    <div>
+      <UserList items={users} />
+    </div>
+  );
+};
+
+export default Users;
